8551e43fc75b766b49740c52d430ad87b50d981e
nyckoben <nyckoben_on_mbxp@users.noreply.github.com>
2024-04-11 00:48:19 +0800

    feat: hello 程序

---
+++ docs/cmd.md
@@ -0,0 +1,9 @@
+# 命令行相关
+
+## 随手记
+* 初始化/编译/运行
+    * go run .\main.go
+    * go mod init gogogo
+    * go mod tidy
+    * go build
+    * .\gogogo.exe
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// go run .\main.go
+// go mod init gogogo
+// go mod tidy
+// go build
+// .\gogogo.exe
+func main() {
+	fmt.Println("Hello, pancake!")
+}
