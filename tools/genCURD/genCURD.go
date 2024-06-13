c0287b2b7888016bdfe2c9b1743a2505388e67ab
nycko <nycko@sina.com>
2024-06-14 01:12:35 +0800

    clean: 删除已经实现并集成的 demo 代码，准备开发 curd 的代码生成工具

---
+++ tools/genCURD/genCURD.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	//1: 读取数据库表结构
+
+	//2: 读取模板文件
+
+	//3: 生成代码
+	//3.1: 调用 gorm-gen 生成 model query 代码
+	//3.2: 生成 curd 的 dao 代码
+	//3.3: 生成 curd 的 proto 定义，包括数据结构和接口定义
+	//3.4: 调用 protoc 生成 go 代码
+	//3.5: 生成 curd 的 service 代码，包括 DO 和 DTO 的转换代码，基础 curd 的实现
+}
